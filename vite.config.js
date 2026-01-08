040603e48281bc803d8b5a642e57192cf388a6b6
THANUJ-IMAYAVARAMBAN31 <varamban2007@gmail.com>
2026-01-08 06:00:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/useFetch-React/",
+  base: "/useFetch-react/",
   plugins: [react()]
 });
